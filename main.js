58bdaa65941ea6d4b68fcc1cfc97165653312172
bryukvahunter <travel4head@gmail.comgit config --global user.name bryukvahuntergit commit -m >
2024-10-16 11:30:17 +0300

    First commit

---
+++ main.js
@@ -0,0 +1,17 @@
+let temperature = +prompt('Какая температура?','');
+
+if (temperature < 0 && temperature > -10) {
+    alert('{Холодновато}');
+} else if (temperature <= -10 && temperature >= -30) {
+    alert('Мёрзнет жопа');
+} else if (temperature <= -31 && temperature >= -100000000) {
+    alert('Парень, ты в Норильске?!');
+} else if (temperature >= 0 && temperature <= 5) {
+    alert('Оттепель...');
+} else if (temperature >= 5 && temperature <= 20) {
+    alert('Запахло весной!');
+} else {
+    alert('Потные подмышки');
+}
+
+console.log(1 + 1);
