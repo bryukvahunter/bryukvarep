8b319a8a5e810d7b8edc6b045b246edce4588196
bryukvahunter <travel4head@gmail.comgit config --global user.name bryukvahuntergit commit -m >
2024-10-16 17:41:36 +0300

    10

--- main.js
+++ main.js
@@ -14,4 +14,62 @@
 //     alert('Потные подмышки');
 // }
 
-let percent;
+
+
+
+
+// let cash = prompt('Введите сумму денег', '');
+
+// if (cash > 1000 && cash < 5000) {
+//     alert(cash - (cash * 0.05));
+// } else if (cash > 5000) {
+//     alert(cash - (cash * 0.1));
+// }
+
+
+
+
+
+// let age = prompt('Введите возраст', '');
+
+// let young = age < 18 && age > 0;
+// let adult = age >= 18 && age <= 65;
+// let old = age > 65;
+
+// if (young) {
+//     alert('Зелен');
+// } else if (adult) {
+//     alert('Созрел маленький');
+// } else if(old) {
+//     alert('Переспелый');
+// } else if (young == '' || young == null) {
+//     alert('Отменено');
+// }
+
+
+
+
+
+// let hour = prompt('Который час?', '');
+
+// if (hour >= 9 && hour <= 18) {
+//     alert('The store is open ');
+// } else {
+//     alert('Closed');
+// }
+
+
+
+
+
+let result = prompt('Результат теста', '');
+
+if (result >= 90 && result <= 100) {
+    alert('Отлично');
+} else if (result >= 70 && result <= 89) {
+    alert('Хорошо');
+} else if (result >= 50 && result <= 69) {
+    alert('Удовлетворительно');
+} else if (result > 0 && result <= 49 ) {
+    alert('Неудачно');
+}
