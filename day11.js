4250283636e6931b93d904f16798f8e22bc4dda4
bryukvahunter <travel4head@gmail.comgit config --global user.name bryukvahuntergit commit -m >
2024-10-18 11:15:37 +0300

    12

--- day11.js
+++ day11.js
@@ -1,15 +1,32 @@
-function calc(operation, a, b) {
-    if (operation ==='add') {
-        return a + b;
-    } 
-    if (operation === 'multi') {
-        return a * b;
-    }
-    if (operation === 'substract') {
-        return a - b;
-    }
-}
+// function calc(operation, a, b) {
+//     if (operation ==='add') {
+//         return a + b;
+//     } 
+//     if (operation === 'multi') {
+//         return a * b;
+//     }
+//     if (operation === 'substract') {
+//         return a - b;
+//     }
+// }
  
-console.log(calc('add',1, 2));
-console.log(calc('multi',1, 2));
-console.log(calc('substract',3, 2));
+// console.log(calc('add',1, 2));
+// console.log(calc('multi',1, 2));
+// console.log(calc('substract',3, 2));
+
+
+
+
+function calc(operation, a, b) {
+    switch (operation) {
+        case 'add':
+            return a + b;
+        case 'multi':
+            return a * b;     
+        case 'substract':
+            return a - b;
+    }   
+}
+console.log(calc('add',5, 5));
+console.log(calc('multi',5, 5));
+console.log(calc('substract',5, 5));
