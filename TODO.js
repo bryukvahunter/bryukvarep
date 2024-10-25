06d46c9a3fc72ccd301ff8bb6ba492104e923c76
Igor Panov <igorpanov@pvh.com>
2024-10-25 13:45:32 +0300

    code review

--- TODO.js
+++ TODO.js
@@ -1,80 +1,84 @@
 const STATUSES = {
-    IN_PROGRESS: 'In Progress',
-    TO_DO: 'To Do',
-    DONE: 'Done'
+  IN_PROGRESS: "In Progress",
+  TO_DO: "To Do",
+  DONE: "Done",
 };
 
 const WARNINGS = {
-    TASK_IN_LIST: 'The task is already on the list',
-    TASK_NOT_FOUND: 'Task not found',
-    INCORRECT_STATUS: 'Incorrect status',
+  TASK_IN_LIST: "The task is already on the list",
+  TASK_NOT_FOUND: "Task not found",
+  INCORRECT_STATUS: "Incorrect status",
 };
 
-
 const toDo = {
-    'make breakfast': STATUSES.DONE,
-    'do the task': STATUSES.IN_PROGRESS,
-    'go to the GYM': STATUSES.TO_DO,
-    'feed the cat': STATUSES.TO_DO,
-    'sell bitcoin': STATUSES.DONE,
-    'buy a yacht': STATUSES.IN_PROGRESS,
+  "make breakfast": STATUSES.DONE,
+  "do the task": STATUSES.IN_PROGRESS,
+  "go to the GYM": STATUSES.TO_DO,
+  "feed the cat": STATUSES.TO_DO,
+  "sell bitcoin": STATUSES.DONE,
+  "buy a yacht": STATUSES.IN_PROGRESS,
 };
 // console.log(toDo);
 
-
 //Проверка на дублирование таски
 function isValue(key) {
-    if (key in toDo) {
-       return true;
-    } else {
-        return false;
-    }
+  return key in toDo;
+  // if (key in toDo) {
+  //    return true;
+  // } else {
+  //     return false;
+  // }
 }
-
 
 function addTask(task) {
-    if (isValue(task) === true) {
-        console.log(WARNINGS.TASK_IN_LIST);
-    } else {
-        toDo[task] = STATUSES.TO_DO;
-    }
+  if (isValue(task) === true) {
+    console.log(WARNINGS.TASK_IN_LIST);
+  } else {
+    toDo[task] = STATUSES.TO_DO;
+  }
 }
 
-
 function changeStatus(task, status) {
-    if (task in toDo) {
-        if (status === STATUSES.IN_PROGRESS) {
-            toDo[task] = status; 
-        }   else if (status === STATUSES.TO_DO) {
-            toDo[task] = status;
-        }   else if (status === STATUSES.DONE) {
-            toDo[task] = status;
-        }   else {
-            console.log(WARNINGS.INCORRECT_STATUS);
-        }   
-        } else {
-            console.log(WARNINGS.TASK_NOT_FOUND);
-        } 
+  if (!isValue(task)) {
+    console.log(WARNINGS.TASK_NOT_FOUND);
+    return;
+  }
+
+  //   const isCorrectStatus = Object.values(STATUSES).includes(status);
+
+  const isCorrectStatus =
+    status === STATUSES.IN_PROGRESS ||
+    status === STATUSES.TO_DO ||
+    status === STATUSES.DONE;
+
+  if (!isCorrectStatus) {
+    console.log(WARNINGS.INCORRECT_STATUS);
+    return;
+  }
+
+  toDo[task] = status;
 }
 
 // changeStatus('make breakfast', STATUSES.DONE);
 
 function deleteTask(task) {
-    if (isValue(task) === true) {
-         delete toDo[task];
-    } else {
-        console.log(WARNINGS.TASK_NOT_FOUND)
-    }
+  // переписать с одним if используя return, без else
+  if (isValue(task)) {
+    delete toDo[task];
+  } else {
+    console.log(WARNINGS.TASK_NOT_FOUND);
+  }
 }
 
 function showList() {
-    for (key1 in toDo) {
-        if (toDo[key1] === STATUSES.TO_DO) {
-            console.log(key1);
-        }
-        
+  for (key1 in toDo) {
+    if (toDo[key1] === STATUSES.TO_DO) {
+      console.log(key1);
     }
+  }
 }
 
+changeStatus("feed the cat", "sdasd");
+console.log(toDo);
 
 showList();
