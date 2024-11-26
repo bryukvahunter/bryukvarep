21557c5297fa0d98680492244ff97fc0dd971abb
Igor Panov <igorpanov@pvh.com>
2024-11-26 11:53:29 +0300

    REVIEW

--- main.js
+++ main.js
@@ -1,55 +1,53 @@
-
 // const allClearBtn = document.getElementById('allClearButton');
 // const clearBtn = document.getElementById('clearButton');
+// можно добавить функцию получения по id + сделать пространство имен
 
-const firstInput = document.getElementById('calcFirstInput');
-const secondInput = document.getElementById('calcSecondInput');
+const getElementById = (id) => {
+  return document.getElementById(id);
+};
+
+const firstInput = getElementById("calcFirstInput");
+const secondInput = getElementById("calcSecondInput");
 // let valFirst = firstInput.value;     // Не работают вне функции
-// let valSecond = secondInput.value; 
+// let valSecond = secondInput.value;
 
-const equalBtn = document.getElementById('equalButton');
-const selectOperator = document.getElementById('calcSelect');
-let result = document.getElementById('result');
-
-
+const equalBtn = getElementById("equalButton");
+const selectOperator = getElementById("calcSelect");
+let result = getElementById("result");
 
 function getResult() {
-    
-    let valFirst = +firstInput.value;
-    let valSecond = +secondInput.value;
-    
-    // if (selectOperator.value === 'subtract') {
-    //     result.innerHTML += valFirst - valSecond;
-    // }
+  // Number()
 
-    switch(selectOperator.value) {
+  let valFirst = Number(firstInput.value);
+  let valSecond = Number(secondInput.value);
 
-        case 'add': 
-        result.innerHTML += valFirst + valSecond;
-        break;
+  // if (selectOperator.value === 'subtract') {
+  //     result.innerHTML += valFirst - valSecond;
+  // }
 
-        case 'subtract': 
-        result.innerHTML += valFirst - valSecond;
-        break;
+  // очищаем innerHTML
+  result.innerHTML = "";
 
-        case 'multi': 
-        result.innerHTML += valFirst * valSecond;
-        break;
+  switch (selectOperator.value) {
+    case "add":
+      result.innerHTML = valFirst + valSecond;
+      break;
 
-        case 'divis': 
-        result.innerHTML += valFirst / valSecond;
-        break;
+    case "subtract":
+      result.innerHTML = valFirst - valSecond;
+      break;
 
-        default: 
-        alert('Try again');
-    }
-    
+    case "multi":
+      result.innerHTML = valFirst * valSecond;
+      break;
+    // проверка на 0
+    case "divis":
+      result.innerHTML = valFirst / valSecond;
+      break;
+
+    default:
+      alert("Try again");
+  }
 }
 
-equalBtn.addEventListener('click', getResult);
-
-
-
-
-
-
+equalBtn.addEventListener("click", getResult);
