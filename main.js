4da02ed571295b9cb902aebc633b50f6f65e0bca
Igor Panov <igorpanov@pvh.com>
2024-12-18 22:16:14 +0300

    review

--- main.js
+++ main.js
@@ -1,66 +1,62 @@
 const getElementById = (id) => {
-    return document.getElementById(id);
+  return document.getElementById(id);
 };
 
 const ID = {
-    FIRST_INPUT: getElementById('input1'),
-    SECOND_INPUT: getElementById('input2'),
-    EQUAL_BTN: getElementById('equalBaton'),
-    SELECT_OPERATOR: getElementById('select'),
-    RESULT: getElementById('result'),
+  FIRST_INPUT: getElementById("input1"),
+  SECOND_INPUT: getElementById("input2"),
+  EQUAL_BTN: getElementById("equalBaton"),
+  SELECT_OPERATOR: getElementById("select"),
+  RESULT: getElementById("result"),
 };
-
 
 function getResult() {
-    
-    let firstValue = Number(ID.FIRST_INPUT.value);
-    let secondValue = Number(ID.SECOND_INPUT.value);
-    
-    ID.RESULT.innerHTML = '';
+  let firstValue = Number(ID.FIRST_INPUT.value);
+  let secondValue = Number(ID.SECOND_INPUT.value);
 
-    switch(ID.SELECT_OPERATOR.value) {
+  ID.RESULT.innerHTML = "";
 
-        case 'add': 
-            ID.RESULT.innerHTML = firstValue + secondValue;
-            break;
+  switch (ID.SELECT_OPERATOR.value) {
+    case "add":
+      ID.RESULT.innerHTML = firstValue + secondValue;
+      break;
 
-        case 'subtract': 
-            ID.RESULT.innerHTML = firstValue - secondValue;
-            break;
+    case "subtract":
+      ID.RESULT.innerHTML = firstValue - secondValue;
+      break;
 
-        case 'multi': 
-            ID.RESULT.innerHTML = firstValue * secondValue;
-            break;
+    case "multi":
+      ID.RESULT.innerHTML = firstValue * secondValue;
+      break;
 
-        case 'divis': 
-            switch(firstValue) {
-                case 0:
-                    alert('На ноль делить нельзя');
-                    break;
-            }
-            ID.RESULT.innerHTML = firstValue / secondValue;
-            break;
+    case "divis":
+      // Свич избыточен, можно if
+      switch (firstValue) {
+        case 0:
+          alert("На ноль делить нельзя");
+          break;
+      }
+      ID.RESULT.innerHTML = firstValue / secondValue;
+      break;
 
-        default: 
-            alert('Try again');
-    }
-    
-};
-
-ID.EQUAL_BTN.addEventListener('click', getResult);
-
-
-const body = document.querySelector('body');
-
-function saveRes() {
-    body.insertAdjacentHTML('beforeend', `<div>${ID.RESULT.innerHTML}</div>`);
+    default:
+      alert("Try again");
+  }
 }
 
-ID.EQUAL_BTN.addEventListener('click', saveRes);
+ID.EQUAL_BTN.addEventListener("click", getResult);
 
+const body = document.querySelector("body");
+
+// придумать как innerHTML и insertAdjacentHTML заменить на textContent везде
+function saveRes() {
+  body.insertAdjacentHTML("beforeend", `<div>${ID.RESULT.innerHTML}</div>`);
+}
+
+ID.EQUAL_BTN.addEventListener("click", saveRes);
 
 function deleteSaved(res) {
-    body.removeChild(res.target);
+  body.removeChild(res.target);
 }
 
-body.addEventListener('click', deleteSaved);
+body.addEventListener("click", deleteSaved);
