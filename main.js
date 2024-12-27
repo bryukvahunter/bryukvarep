fd826ed8dcc21c2710b5750232ec946ef36d2b5b
bryukvahunter <travel4head@gmail.comgit config --global user.name bryukvahuntergit commit -m >
2024-12-27 17:32:58 +0300

    27.12.2024

--- main.js
+++ main.js
@@ -1,66 +1,179 @@
-const getElementById = (id) => {
-    return document.getElementById(id);
+
+const taskFormHigh = document.querySelector('#add-form-high');
+const taskFormLow = document.querySelector('#add-form-low');
+const inputHigh = document.querySelector('#input-high');
+const inputLow = document.querySelector('#input-low');
+const buttonAdd = document.querySelector('#btn');
+const commonBlockHigh = document.querySelector('#ul-block-high');
+const commonBlockLow = document.querySelector('#ul-block-low');
+
+const storage = [];
+
+const STATUSES = {
+    TO_DO: 'To do',
+    DONE: 'Done',
+};
+const PRIORITIES = {
+    HIGH: 'High',
+    LOW: 'Low',
 };
 
-const ID = {
-    FIRST_INPUT: getElementById('input1'),
-    SECOND_INPUT: getElementById('input2'),
-    EQUAL_BTN: getElementById('equalBaton'),
-    SELECT_OPERATOR: getElementById('select'),
-    RESULT: getElementById('result'),
-};
 
 
-function getResult() {
+taskFormHigh.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let input = inputHigh.value;
+    const newTask = {
+        taskName: input,
+        taskPriority: PRIORITIES.HIGH,
+        taskStatus: STATUSES.TO_DO,
+    };
+    storage.push(newTask);
+    console.log(storage);
+    clean();
+    render();
     
-    let firstValue = Number(ID.FIRST_INPUT.value);
-    let secondValue = Number(ID.SECOND_INPUT.value);
+    e.target.reset();
+})
+
+taskFormLow.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let input = inputLow.value;
+    const newTask = {
+        taskName: input,
+        taskPriority: PRIORITIES.LOW,
+        taskStatus: STATUSES.TO_DO,
+    };
+    storage.push(newTask);
+    console.log(storage);
+    clean();
+    render();
     
-    ID.RESULT.innerHTML = '';
+    e.target.reset();
+})
 
-    switch(ID.SELECT_OPERATOR.value) {
 
-        case 'add': 
-            ID.RESULT.innerHTML = firstValue + secondValue;
-            break;
+function createElement(text, taskStatus) {
+    const newBlockLi = document.createElement('li');
+    const newBlockInput = document.createElement('input');
+    const newBlockP = document.createElement('p');
+    const newBlockButton = document.createElement('button');
+    
+    newBlockInput.type = 'checkbox';
+    newBlockInput.classList.add('checkbox');
+    newBlockP.classList.add('task-name', 'font-size');
+    newBlockButton.classList.add('baton-close');
+    newBlockButton.setAttribute('id', 'btn-close');
+    newBlockLi.setAttribute('id', 'taska');
+    
+    newBlockLi.appendChild(newBlockInput);
+    newBlockLi.appendChild(newBlockP);
+    newBlockLi.appendChild(newBlockButton);
 
-        case 'subtract': 
-            ID.RESULT.innerHTML = firstValue - secondValue;
-            break;
+    newBlockP.textContent = text;
 
-        case 'multi': 
-            ID.RESULT.innerHTML = firstValue * secondValue;
-            break;
-
-        case 'divis': 
-            switch(firstValue) {
-                case 0:
-                    alert('На ноль делить нельзя');
-                    break;
-            }
-            ID.RESULT.innerHTML = firstValue / secondValue;
-            break;
-
-        default: 
-            alert('Try again');
+    if (taskStatus === STATUSES.DONE) {
+        newBlockInput.checked = 'checked';   
     }
+
+    newBlockButton.addEventListener('click', function deleteTask () {
+        const taskText = newBlockLi.querySelector('.task-name').textContent;
+        const findIndex = storage.findIndex(index => {
+            return index.taskName === taskText;
+        });
+        storage.splice(findIndex, 1);
+        clean();
+        render();
+    });
+
+    newBlockInput.addEventListener('change', function doTask () {
+        const taskText = newBlockLi.querySelector('.task-name').textContent;
+        const findIndex = storage.findIndex(index => {
+            return index.taskName === taskText;
+        });
+        this.checked ? storage[findIndex].taskStatus = STATUSES.DONE : storage[findIndex].taskStatus = STATUSES.TO_DO;
+        console.log(storage);
+    })
+
+    return newBlockLi;
+}
+
+function render() {
+    storage.forEach(item => {
+        if(item.taskPriority === 'High') {
+            commonBlockHigh.appendChild(createElement(item.taskName, item.taskStatus));
+            return;
+        }
+        if(item.taskPriority === 'Low') {
+            commonBlockLow.appendChild(createElement(item.taskName, item.taskStatus));
+            return;
+        }
+    })
     
 };
 
-ID.EQUAL_BTN.addEventListener('click', getResult);
-
-
-const body = document.querySelector('body');
-
-function saveRes() {
-    body.insertAdjacentHTML('beforeend', `<div>${ID.RESULT.innerHTML}</div>`);
+function clean() {
+    let allTask = document.querySelectorAll('#taska');
+    for(let i = 0; i < allTask.length; i++) {
+        allTask[i].remove();
+    }
 }
 
-ID.EQUAL_BTN.addEventListener('click', saveRes);
 
 
-function deleteSaved(res) {
-    body.removeChild(res.target);
-}
 
-body.addEventListener('click', deleteSaved);
+
+
+
+// Клонирование блока
+
+// const liBlock = document.querySelector('#task-block');  
+// const ulBlock = document.querySelector('#high-blocks');
+// const cloneBlock = liBlock.cloneNode(true);
+//     ulBlock.appendChild(cloneBlock);
+// // cloneBlock.innerHTML = `${val}`;
+
+
+// Колхоз
+
+            // `<li>
+            //     <lable>
+            //         <input class="checkbox-high" type="checkbox">
+            //     </lable>
+            //         <p class="task-name font-size">${val}</p>
+            //         <button class="baton-close"></button>
+            // </li>`
+
+
+
+// Создание нового элемента
+
+// const addTaskForm = document.getElementById('add-form');
+// const ulBlock = document.querySelector('#high-blocks');
+
+// function createElement(event) {
+//     event.preventDefault();
+
+//     const ulBlock = document.querySelector('#high-blocks');
+//     const newBlockLi = document.createElement('li');
+//     const newBlockLabel = document.createElement('label');
+//     const newBlockInput = document.createElement('input');
+//     const newBlockP = document.createElement('p');
+//     const newBlockButton = document.createElement('button');
+    
+//     newBlockInput.type = 'checkbox'
+//     newBlockP.classList.add('task-name');
+//     newBlockButton.classList.add('baton-close');
+ 
+//     newBlockLabel.appendChild(newBlockInput);
+//     newBlockLi.appendChild(newBlockLabel);
+//     newBlockLi.appendChild(newBlockP);
+//     newBlockLi.appendChild(newBlockButton);
+
+//     ulBlock.appendChild(newBlockLi);
+
+//     event.target.reset();
+// }
+
+// addTaskForm.addEventListener('submit', createElement);
+    
